f4f83623576a97285face34065c7d63c2f16612e
sash04ek <a.ovcharov@afterlogic.com>
2023-07-06 13:24:14 +0300

    fixed - BUG - attachments with ';' are not supported

--- lib/MailSo/Mime/ParameterCollection.php
+++ lib/MailSo/Mime/ParameterCollection.php
@@ -95,7 +95,7 @@ class ParameterCollection extends \MailSo\Base\Collection
     {
         $this->clear();
 
-        $aDataToParse = explode(';', $sRawParams);
+        $aDataToParse = preg_split('/\s*;\s*(?=(?:[^"]*"[^"]*")*[^"]*$)/', $sRawParams);
 
         foreach ($aDataToParse as $sParam) {
             $this->Add(Parameter::CreateFromParameterLine($sParam));
